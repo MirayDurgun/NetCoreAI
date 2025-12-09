5df733b4b405b77d35953ba7fbfe08037b9a5b81
miray <miraydurgun67@gmail.com>
2025-12-09 22:43:40 +0300

    B-1 Ders 7 tamamlandı

---
+++ NetCoreAI.Project01_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project01_ApiDemo.Entities;
+using System.Runtime.ConstrainedExecution;
+
+namespace NetCoreAI.Project01_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;initial catalog=ApiAIDb;integrated security=true;trustservercertificate=true");
+            //trustservercertificate=true     Sunucu sertifikası doğrulanmadan da bağlantıya izin ver
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
