228f99cebca5c30a616104531ac7cc254cc5f995
miray <miraydurgun67@gmail.com>
2025-12-12 20:43:01 +0300

    B-2 Ders 10

---
+++ NetCoreAI.Project2_ApiConsumeUI/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAI.Project2_ApiConsumeUI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult CustomerList()
+        {
+            return View();
+        }
+    }
+}
