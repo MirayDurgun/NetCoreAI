3f69d7924c8ee5c3cb4d4e2201adcade60704308
miray <miraydurgun67@gmail.com>
2025-12-15 00:46:38 +0300

    B-2 Ders 11

--- NetCoreAI.Project2_ApiConsumeUI/Controllers/CustomerController.cs
+++ NetCoreAI.Project2_ApiConsumeUI/Controllers/CustomerController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAI.Project2_ApiConsumeUI.Dtos;
+using Newtonsoft.Json;
 
 namespace NetCoreAI.Project2_ApiConsumeUI.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult CustomerList()
+        private readonly IHttpClientFactory _httpClientFactory;
+        //HttpClient:  Başka bir sisteme internet üzerinden istek atan şey
+        //IHttpClientFactory: Bu istek atan şeyleri güvenli ve doğru şekilde veren sistem
+        public CustomerController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        //IActionResult Basit, anlık biten işler için kullanılır. (veri tabanı sorgusu veya uzun işlemlerde kullanılmaz) Çünkü sunucunun verimini düşürür.
+        //Asenkron Veritabanı, Harici API, Dosya Okuma gibi bekleme içeren işler için kullanılır.
+        public async Task<IActionResult> CustomerList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44344/api/Customers");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //responsemesage içindeki değeri okuyup jsonData'ya atıyor
+                var values = JsonConvert.DeserializeObject<List<ResultCustomerDto>>(jsonData);
+                //Json formatındaki veriyi string olarak alıp List<ResultCustomerDto> formatına çeviriyor
+                return View(values);
+            }
             return View();
         }
     }
