fcd54ceab887b927f6b43e5b5a49775ea6aac33d
MirayDurgun <miraydurgun67@gmail.com>
2025-12-15 17:16:36 +0300

    B-3 Ders 17 View model oluşturma

---
+++ NetCoreAI.Project3_RapidApi/ViewModels/ApiSeriesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreAI.Project3_RapidApi.ViewModels
+{
+    public class ApiSeriesViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+        public string big_image { get; set; }
+        public string[] genre { get; set; }
+        public float rating { get; set; }
+        public string year { get; set; }
+        public string imdbid { get; set; }
+    }
+}
