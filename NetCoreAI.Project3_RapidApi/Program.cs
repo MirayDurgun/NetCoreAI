3ed183c1ff536727bc244593a1a000bd6f250168
miray <miraydurgun67@gmail.com>
2025-12-18 15:23:35 +0300

    B-4 Ders 21

--- NetCoreAI.Project3_RapidApi/Program.cs
+++ NetCoreAI.Project3_RapidApi/Program.cs
@@ -5,11 +5,15 @@ using System.Net.Http.Headers;
 
 
 var client = new HttpClient();
+
+//API'den gelecek dizi listesini hafızada tutmak için boş bir liste oluşturuluyor
 List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+
+//apiye gidecek istek detayları
 var request = new HttpRequestMessage
 {
     Method = HttpMethod.Get,
-    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"), //istek atılacak adres
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"), //istek atılacak adres, veri alınacak get
     Headers = //istek atılan kaynak 
     {
         { "x-rapidapi-key", "0d63638b74mshf218c5b70b03499p1ae6bbjsn7318835503c5" }, //key
@@ -20,6 +24,8 @@ using (var response = await client.SendAsync(request))
 {
     response.EnsureSuccessStatusCode();
     var body = await response.Content.ReadAsStringAsync();
+
+    //JSON formatındaki ham metni, C# dilinde dönüştürme
     apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
     foreach (var series in apiSeriesViewModels)
     { Console.WriteLine("Film Adı" + series.title + "Film Puanı" + series.rating + "-" + series.rank + "Yıl" + series.year); }
--- NetCoreAI.Project4_OpenAIChat/Program.cs
+++ NetCoreAI.Project4_OpenAIChat/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        var apiKey = "api key";
+
+        Console.WriteLine("Merhaba, size nasıl yarımcı olabilirim?");
+
+        var prompt = Console.ReadLine();
+
+        //internet üzerinden veri alışverişi OpenAI Chat API'sine istek gönderme
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+        //istek gövdesi, API'ye gönderilecek JSON formatındaki ayarlar ve mesaj içeriği
+        var requestBody = new
+        {
+            model = "gpt-3.5-turbo", //kullanılıcak yapay zeka modeli
+            messages = new[]
+            {
+                new {role="system", content="You are a helpful assistant."}, //sistem mesajı, yapay zekanın davranışını belirler
+                new {role="user",content =prompt} //kullanıcı mesajı, kullanıcının girdiği metin
+            },
+            max_tokens = 100 //Cevabın uzunluğunu sınırlar; maliyet ve performans kontrolü için
+        };
+
+        await Task.CompletedTask;
+    }
+}
