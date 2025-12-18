8196ba96bcad37ba9a0931e793c5d1dc3ba03e6a
miray <miraydurgun67@gmail.com>
2025-12-18 20:51:26 +0300

    B-5 Ders 27

---
+++ NetCoreAI.Project5_OpenAIWhisperAudioTranskript/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http.Headers;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var apiKey = "key";
+        var audioFilePath = "sarzamanimizigeriye.mp3";
+
+        using(var httpClient = new HttpClient())
+        {
+            httpClient.DefaultRequestHeaders.Authorization=new AuthenticationHeaderValue("Bearer", apiKey);
+        }
+}
