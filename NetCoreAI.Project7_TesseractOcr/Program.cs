a5507a29e02484b6a6d78e4df9048fe1b39a9e32
miray <miraydurgun67@gmail.com>
2026-01-30 19:49:38 +0300

    B-7 Ders 35

--- NetCoreAI.Project7_TesseractOcr/Program.cs
+++ NetCoreAI.Project7_TesseractOcr/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Tesseract;
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Karakter okuması için görsel yolu giriniz:");
+    string imagePath = Console.ReadLine();
+    string tessDataPath = @"C:\tessdata";
+
+    try
+    {
+        using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default)
+        {
+            using (var img = Pix.LoadFromFile(imagePath))
+        {
+            using (var page = engine.Process(img))
+            {
+                using (var page = engine.Process(img))
+                {
+                    string text = page.GetText();
+                    float confidence = page.GetMeanConfidence() * 100;
+                    Console.WriteLine("Okunan Metin:\n" + text);
+                    Console.WriteLine($"Güven Skoru: {confidence:F2}%");
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Hata oluştu: " + ex.Message);
+    }
+}
